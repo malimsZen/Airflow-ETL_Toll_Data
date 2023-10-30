9ca1b445a9cfb9451b79fc9fcf7b3bffbd8932bc
malims_zen <75028470+malimsZen@users.noreply.github.com>
2023-10-30 14:53:44 +0300

    Update ETL_toll_data.py
    Removal of comment

--- ETL_toll_data.py
+++ ETL_toll_data.py
@@ -68,4 +68,3 @@ consolidate_data=BashOperator(
 #Task pipeline
 download_data >> unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data
 
-# Airflow repoository.
